314b564bf7dbdfb7e5dc09641d168c5ec3246011
Eric Cheng <echeng@scu.edu>
2021-04-26 15:55:29 -0700

    Add cognito server and client

---
+++ src/main/java/org/dev/authentication/Response.java
@@ -0,0 +1,37 @@
+package org.dev.authentication;
+
+
+import java.util.Map;
+
+public class Response {
+
+    private int statusCode;
+    private String body;
+    private Map<String, String> headers;
+
+    public Response(){}
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(Map<String, String> headers) {
+        this.headers = headers;
+    }
+}
